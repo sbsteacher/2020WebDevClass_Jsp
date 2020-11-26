fddcfaf7dc98147d4257fa886b9fdae591a75505
Axiaguard <admin@axiaguard.com>
2020-11-26 21:11:53 +0900

    디테일 작업 완료

---
+++ board/WebContent/WEB-INF/jsp2/boardDetail.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>디테일</title>
+</head>
+<body>
+	<div>
+		<div>번호 : ${item.i_board}</div>
+		<div>제목 : ${item.title }</div>
+		<div>내용 : ${item.ctnt }</div>
+		<div>제목 : ${item.r_dt }</div>
+	</div>
+</body>
+</html>
--- board/src/com/koreait/board/v2/Utils.java
+++ board/src/com/koreait/board/v2/Utils.java
@@ -12,7 +12,25 @@ public class Utils {
 		String jsp = String.format("/WEB-INF/jsp2/%s.jsp", target);
 		request.getRequestDispatcher(jsp).forward(request, response);
 	}
+	
+	public static int getIntParam(HttpServletRequest request, String key) {
+		String param = request.getParameter(key);
+		return parseStrToInt(param);
+	}
+	
+	public static int parseStrToInt(String val) {
+		try {
+			return Integer.parseInt(val);
+		} catch(Exception e) {}
+		return 0;
+	}
 }
+
+
+
+
+
+
 
 
 
